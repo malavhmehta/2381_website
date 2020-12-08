ed410170f8124952b202472cb4c252301fbca382
malav-mehta <malav.mehta@hotmail.com>
2020-12-07 22:52:20 -0500

    fixed contact

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -85,10 +85,8 @@ export default function Contact() {
       <iframe
         width="100%"
         height="600"
-        frameborder="0"
+        frameBorder="0"
         scrolling="no"
-        marginheight="0"
-        marginwidth="0"
         src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=2381%20Ogilvie%20Road+(Colonel%20By%20Secondary%20School)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
       ></iframe>
       <div className="my-2 py-2"></div>
